deae3c94121a3dac6c50fa7b611a3e5646f44b14
Travis Grubbs <flygrubbs@yahoo.com>
2019-03-05 19:03:25 -0500

    projectsadded

---
+++ client/src/components/Projects.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Projects extends Component {
+
+    render() {
+
+        return(
+
+            <div>
+                projects
+            </div>
+        )
+
+    }
+}
